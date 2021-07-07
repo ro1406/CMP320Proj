f4a55d93e8b51c50db707617191f0aeb7ce64ad1
Rohan <rohanmitra8@gmail.com>
2021-07-07 20:02:26 +0400

    Initial comit

---
+++ CourseProject/src/UniSystem/Driver.java
@@ -0,0 +1,12 @@
+package UniSystem;
+
+/**
+ *
+ * @author Rohan
+ */
+public class Driver {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        (new LoginForm()).setVisible(true);
+    }
+}
