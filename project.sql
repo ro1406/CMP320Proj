0bdcb077361d2237cda9f08506a029836cf56b04
sachf <47702700+sachf@users.noreply.github.com>
2021-07-09 12:05:48 +0400

    Add files via upload

--- project.sql
+++ project.sql
@@ -5,26 +5,60 @@ drop table courses_sections;
 drop table courses_prerequisites;
 drop table professors_courses;
 drop table students_grades;
+drop table users;
+
+create table users (
+    username varchar2(25)           constraint usr_pk primary key,
+    password varchar2 (25),
+    name varchar2 (25),
+    type number(1)                  constraint usr_num_chk check (type between 0 and 2)
+);
+
+insert into users values ('admin','adminpwd','Adminstrator',0);
 
 create table students(
     sid             int             constraint std_pk primary key,
     name            varchar2(50)    constraint std_name_nn not null,
     sex             char            constraint std_sex_chk check (sex in ('M','F')),
     start_sem       varchar2(25)    constraint std_sem_chk check (REGEXP_LIKE(start_sem,'[Fall|Spring|Summer] [0-9][0-9][0-9][0-9]')),
-    credits         int             constraint std_credits_chk check (credits between 0 and 200)
+    college         varchar2(4)     constraint std_dept_chk check (college in ('CAAD','SBA','CAS','CEN')),
+    credits         int             constraint std_credits_chk check (credits between 0 and 200),
+    standing        varchar2(25)    constraint std_stand_chk check (standing in ('Freshman','Sophomore','Junior','Senior')),
+    gpa             number(3,2)     constraint std_gpa_chk check (gpa between 0.0 and 4.0)
 );
 
-insert into students values (70000,'Braiden Lane','M','Fall 2020',0);
-insert into students values (71000,'Rajveer Whitehouse','M','Fall 2020',0);
-insert into students values (72000,'Matt Goodman','M','Fall 2020',0);
-insert into students values (73000,'Reyansh Palmer','M','Fall 2020',0);
-insert into students values (74000,'Aahil Jaramillo','M','Fall 2020',0);
-insert into students values (75000,'Najma Galvan','F','Fall 2020',0);
-insert into students values (76000,'Rhona Woods','F','Fall 2020',0);
-insert into students values (77000,'Maria Shaffer','F','Fall 2020',0);
-insert into students values (78000,'Hafsah Gough','F','Fall 2020',0);
-insert into students values (79000,'Mathilda Conway','F','Fall 2020',0);
-insert into students values (80000,'Tester McTester','M','Fall 2019',0);
+insert into users values ('s70000','s70000pwd','Braiden Lane',1);
+insert into students values (70000,'Braiden Lane','M','Fall 2019','CEN',30,'Junior',3.0);
+
+insert into users values ('s71000','s71000pwd','Rajveer Whitehouse',1);
+insert into students values (71000,'Rajveer Whitehouse','M','Fall 2018','CEN',60,'Senior',3.0);
+
+insert into users values ('s72000','s72000pwd','Matt Goodman',1);
+insert into students values (72000,'Matt Goodman','M','Fall 2018','CEN',100,'Senior',2.6);
+
+insert into users values ('s73000','s73000pwd','Reyansh Palmer',1);
+insert into students values (73000,'Reyansh Palmer','M','Spring 2018','CEN',85,'Senior',2.9);
+
+insert into users values ('s74000','s74000pwd','Aahil Jaramillo',1);
+insert into students values (74000,'Aahil Jaramillo','M','Fall 2020','CEN',60,'Sophomore',3.5);
+
+insert into users values ('s75000','s75000pwd','Najma Galvan',1);
+insert into students values (75000,'Najma Galvan','F','Fall 2020','CEN',60,'Junior',3.6);
+
+insert into users values ('s76000','s76000pwd','Rhona Woods',1);
+insert into students values (76000,'Rhona Woods','F','Fall 2020','CEN',60,'Junior',3.1);
+
+insert into users values ('s77000','s77000pwd','Maria Shaffer',1);
+insert into students values (77000,'Maria Shaffer','F','Fall 2021','CEN',30,'Sophomore',2.9);
+
+insert into users values ('s78000','s78000pwd','Hafsah Gough',1);
+insert into students values (78000,'Hafsah Gough','F','Fall 2021','CEN',30,'Sophomore',2.3);
+
+insert into users values ('s79000','s79000pwd','Mathilda Conway',1);
+insert into students values (79000,'Mathilda Conway','F','Spring 2020','CEN',0,'Freshman',0.0);
+
+insert into users values ('s80000','s80000pwd','Tester McTester',1);
+insert into students values (80000,'Tester McTester','M','Spring 2020','CEN',0,'Freshman',0.0);
 
 select * from students;
 
@@ -36,10 +70,19 @@ create table professors(
     age             int             constraint prf_age_chk check (age between 0 and 100)
 );
 
+insert into users values ('p10','p10pwd','Jo Romero',2);
 insert into professors values (10,'Jo Romero','M','CEN',35);
+
+insert into users values ('p20','p20pwd','Evelina Mason',2);
 insert into professors values (20,'Evelina Mason','F','CEN',45);
+
+insert into users values ('p30','p30pwd','Sam Wilkins',2);
 insert into professors values (30,'Sam Wilkins','M','CAS',37);
+
+insert into users values ('p40','p40pwd','Harry Freeman',2);
 insert into professors values (40,'Harry Freeman','M','CAS',40);
+
+insert into users values ('p50','p50pwd','Tayah Allen',2);
 insert into professors values (50,'Tayah Allen','M','SBA',33);
 
 create table courses(
@@ -48,9 +91,9 @@ create table courses(
     credits         int             constraint crs_credits_chk check (credits between 1 and 4)
 );
 
-insert into courses values ('COE101','Introduction to Engineering 1',3);
-insert into courses values ('COE102','Introduction to Engineering 2',4);
-insert into courses values ('COE108','Engineering Project',2);
+insert into courses values ('CEN101','Introduction to Engineering 1',3);
+insert into courses values ('CEN102','Introduction to Engineering 2',4);
+insert into courses values ('CEN108','Engineering Project',2);
 insert into courses values ('CAS101','Introduction to Science 1',3);
 insert into courses values ('CAS102','Introduction to Science 2',4);
 insert into courses values ('CAS108','Science Project',2);
@@ -67,10 +110,10 @@ create table courses_sections(
     time            varchar2(50)    constraint crs_sec_time_nn not null
 );
 
-insert into courses_sections values (11011,'COE101',10,'Summer 2021','EB','8AM - 9AM');
-insert into courses_sections values (11012,'COE101',20,'Summer 2021','EB','10AM - 11AM');
-insert into courses_sections values (12011,'COE102',10,'Fall 2021','EB','12PM - 1PM');
-insert into courses_sections values (12012,'COE102',20,'Fall 2021','EB','2PM - 3PM');
+insert into courses_sections values (11011,'CEN101',10,'Summer 2021','EB','8AM - 9AM');
+insert into courses_sections values (11012,'CEN101',20,'Summer 2021','EB','10AM - 11AM');
+insert into courses_sections values (12011,'CEN102',10,'Fall 2021','EB','12PM - 1PM');
+insert into courses_sections values (12012,'CEN102',20,'Fall 2021','EB','2PM - 3PM');
 
 create table courses_prerequisites(
     course_code     varchar2(10)    constraint crs_preq_crs_fk references courses(course_code),
@@ -78,8 +121,8 @@ create table courses_prerequisites(
                                     constraint crs_preq_crn_pk primary key (course_code,prereq_code)
 );
 
-insert into courses_prerequisites values ('CEN102','COE101');
-insert into courses_prerequisites values ('COE108','COE102');
+insert into courses_prerequisites values ('CEN102','CEN101');
+insert into courses_prerequisites values ('CEN108','CEN102');
 insert into courses_prerequisites values ('CAS102','CAS101');
 insert into courses_prerequisites values ('CAS108','CAS102');
 insert into courses_prerequisites values ('CAAD102','CAAD101');
@@ -91,11 +134,11 @@ create table professors_courses(
                                     constraint prf_crs_pk primary key (pid,course_code)
 );
 
-insert into professors_courses values (10,'COE101');
-insert into professors_courses values (10,'COE102');
-insert into professors_courses values (10,'COE108');
-insert into professors_courses values (20,'COE101');
-insert into professors_courses values (20,'COE102');
+insert into professors_courses values (10,'CEN101');
+insert into professors_courses values (10,'CEN102');
+insert into professors_courses values (10,'CEN108');
+insert into professors_courses values (20,'CEN101');
+insert into professors_courses values (20,'CEN102');
 insert into professors_courses values (30,'CAS101');
 insert into professors_courses values (30,'CAS102');
 insert into professors_courses values (30,'CAS108');
@@ -128,3 +171,9 @@ select * from courses_sections;
 select * from courses_prerequisites;
 select * from professors_courses;
 select * from students_grades;
+select * from users;
+
+commit;
+
+
+SELECT username, password, type FROM USERS WHERE username = 's80000' AND password = 's80000pwd';
